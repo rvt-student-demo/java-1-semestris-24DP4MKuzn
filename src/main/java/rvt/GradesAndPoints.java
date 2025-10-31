b9f783d3b63fa040c569b79670d10bacb8474e6b
Matvejs Kuznecovs <A242564MK@rvt.lv>
2025-10-31 10:37:42 +0200

    GradesAndPoints exercise complete

---
+++ src/main/java/rvt/GradesAndPoints.java
@@ -0,0 +1,30 @@
+package rvt;
+
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter points: ");
+        int points = scanner.nextInt();
+
+        if (points < 0) {
+            System.out.println("Impossible!");
+        } else if (points < 50) {
+            System.out.println("Failed");
+        } else if (points < 60) {
+            System.out.println("1");
+        } else if (points < 70) {
+            System.out.println("2");
+        } else if (points < 80) {
+            System.out.println("3");
+        } else if (points < 90) {
+            System.out.println("4");
+        } else if (points < 100) {
+            System.out.println("5");
+        } else if (points > 100) {
+            System.out.println("Incredible!");
+        }
+    }
+}
