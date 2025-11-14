d9bf3131dedacfef213d07adc3f2af1233c39e44
Matvejs Kuznecovs <A242564MK@rvt.lv>
2025-11-14 10:07:42 +0200

    JMArrayList Task 1 complete

---
+++ src/main/java/rvt/JMArrayList.java
@@ -0,0 +1,117 @@
+package rvt;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class JMArrayList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select an excercise to run (1 to 4): ");
+        int taskToRun = scanner.nextInt();
+
+        switch (taskToRun) {
+            case 1:
+                System.out.println("TASK 1: ");
+                Task1_OnlyTheseNumbers(scanner);
+                break;
+            case 2:
+                System.out.println("TASK 2: ");
+                Task2_ListSize(scanner);
+                break;
+            case 3:
+                System.out.println("TASK 3: ");
+                Task3_OnTheList(scanner);
+                break;
+            case 4:
+                System.out.println("TASK 4: ");
+                Task4_RemoveLast(scanner);
+                break;
+            default:
+                System.out.println("Excercise not found");
+                break;
+        }
+    }
+
+    public static void Task1_OnlyTheseNumbers(Scanner scanner) {
+        boolean stop = false;
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (stop == false) 
+        {
+            System.out.println("Enter a number (-1 to stop)");
+            int input = scanner.nextInt();
+            if (input == -1) 
+            {
+                stop = true;
+            } 
+            else 
+            {
+                numbers.add(input);
+            }
+        }
+
+        System.out.println("You entered: " + numbers);
+
+        System.out.println("Enter indice range...");
+        System.out.println("Start indice: ");
+        int start = scanner.nextInt();
+        System.out.println("End indice: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Values in indice range: " + start + ", " + end);
+        for (int i = start; i <= end; i++) 
+        {
+            System.out.println(numbers.get(i));
+        }   
+    }
+
+    public static void Task2_ListSize(Scanner scanner) {
+        int[] val = {13, -4, 82, 17};
+        int[] twice = {};
+
+        System.out.println("Original Array: "
+                + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
+
+        twice = val;
+        for (int i = 0; i < twice.length; i++) {
+            twice[i] = val[i] * 2;
+        }
+
+        System.out.println("New Array: "
+                + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
+    }
+
+    public static void Task3_OnTheList(Scanner scanner) {
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+    
+    for (int i = 0; i < sum.length; i++) {
+            sum[i] = valA[i] + valB[i];
+        }
+ 
+    System.out.println( "sum: " 
+        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+    }
+
+    public static void Task4_RemoveLast(Scanner scanner) {
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {  0,   0,   0,   0};
+ 
+    valB[0] = 12;
+    valB[1] = 47;
+    valB[2] = -57;
+    valB[3] = 8;
+
+    System.out.println( "valA: " 
+        + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+ 
+    System.out.println( "valB: " 
+        + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+
+    System.out.println( "sum:  " 
+        + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+        + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
+    }
+}
