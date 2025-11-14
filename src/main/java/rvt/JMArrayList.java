385e19ea8ceee111ecb30e3eddf7c3a3398928cb
Matvejs Kuznecovs <A242564MK@rvt.lv>
2025-11-14 10:24:21 +0200

    JMArrayList Task 2 complete

--- src/main/java/rvt/JMArrayList.java
+++ src/main/java/rvt/JMArrayList.java
@@ -67,19 +67,25 @@ public class JMArrayList {
     }
 
     public static void Task2_ListSize(Scanner scanner) {
-        int[] val = {13, -4, 82, 17};
-        int[] twice = {};
-
-        System.out.println("Original Array: "
-                + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
-
-        twice = val;
-        for (int i = 0; i < twice.length; i++) {
-            twice[i] = val[i] * 2;
+        ArrayList<String> names = new ArrayList<>();
+        boolean stop = false;
+        scanner.nextLine();
+        
+        while (stop == false) 
+        {
+            System.out.println("Enter name (enter 'stop' to stop)");
+            String input = scanner.nextLine();
+            if (input.equals("stop")) 
+            {
+                stop = true;
+            } 
+            else 
+            {
+                names.add(input);
+            }
         }
 
-        System.out.println("New Array: "
-                + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
+        System.out.println("Number of names: " + names.size());
     }
 
     public static void Task3_OnTheList(Scanner scanner) {
